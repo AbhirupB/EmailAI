8dc8be22d0116d56caf553443127b894ffa926eb
Abhirup Bhattacharya <abhirupbhattacharya97@gmail.com>
2025-07-16 14:40:08 +0530

    generated response using webClient, extracted response and completed controller method.

--- email-writer-sb/src/main/java/com/email/writer/api/EmailGenerator/EmailGeneratorController.java
+++ email-writer-sb/src/main/java/com/email/writer/api/EmailGenerator/EmailGeneratorController.java
@@ -1,17 +1,24 @@
 package com.email.writer.api.EmailGenerator;
 
 import com.email.writer.api.EmailGenerator.model.EmailRequest;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/email")
 public class EmailGeneratorController {
 
     //EmailRequest is a DTO class
+    private EmailGeneratorService emailGeneratorService;
+
+    @PostMapping("/generate")
     public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest){
-        return ResponseEntity.ok("");
+        String response = emailGeneratorService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
     }
 }
--- email-writer-sb/src/main/java/com/email/writer/api/EmailGenerator/EmailGeneratorService.java
+++ email-writer-sb/src/main/java/com/email/writer/api/EmailGenerator/EmailGeneratorService.java
@@ -1,14 +1,23 @@
 package com.email.writer.api.EmailGenerator;
 
 import com.email.writer.api.EmailGenerator.model.EmailRequest;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Map;
 
 @Service
 public class EmailGeneratorService {
 
+    private final WebClient webClient;
+
+    public EmailGeneratorService(WebClient.Builder webClientBuilder){
+
+        this.webClient = webClientBuilder.build();
+    }
     //we are setting the URL and key as env variables in configuration and in application.properties file
     @Value("${gemini.api.url}")
     private String geminiApiUrl;
@@ -45,11 +54,36 @@ public class EmailGeneratorService {
                                 })
                 }
         );
+
         //Do request and get response
+        String response = webClient.post()
+                .uri(geminiApiUrl + geminiApiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
 
-        //Return response
+        //Extract response from the response structure that we receive from the API in the "text: " part
+        // and return response
 
-        return null;
+        return extractResponseContent(response);
+    }
+
+    private String extractResponseContent(String response) {
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(response);
+            return rootNode.path("candidates")
+                    .get(0)
+                    .path("content")
+                    .path("parts")
+                    .get(0)
+                    .path("text")
+                    .asText();
+        } catch(Exception e){
+            return "Error processing request" + e.getMessage();
+        }
     }
 
     private String buildPrompt(EmailRequest emailRequest) {
